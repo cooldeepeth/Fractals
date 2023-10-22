87707701bceecf01b60f7f1f545fcdfc31ce20a5
Vashishth Soni <vashisoni503@gmail.com>
2023-10-22 12:02:14 +0530

    Update MyFNFT.sol
    isMinter helper function for game contract is created which returns address of minter of specified NFT.
    In unfractionalizeNft function balance will deduct to 0.

--- contracts/MyFNFT.sol
+++ contracts/MyFNFT.sol
@@ -1,16 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity >=0.8.21 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract FNFT is ERC1155 {
-    address private gameContract;
-    
-    function setGameContract(address _addr) external{
-        gameContract = _addr;
-    }
 
-    constructor() ERC1155(""){}
+    constructor() ERC1155("FRACTAL"){}
 
     mapping(uint => bool) public _isExist;
     
@@ -19,7 +14,6 @@ contract FNFT is ERC1155 {
     mapping(uint256 => address) private _Minter;
 
     mapping(uint => bool) public _isInitialized;
-
 
     mapping(address => mapping(uint256 => uint256)) public _balanceOf;
 
@@ -30,20 +24,17 @@ contract FNFT is ERC1155 {
     uint256 private counter;
 
     modifier isExist(uint id){
-        require(_isExist[id],"Id Does not exist");
+        require(_isExist[id],"id Does not exist");
         _;
     }
-    function isMinter(uint256 tokenId) external view returns(address){
-        return _Minter[tokenId];
-    }
-
+    
     function createNFT(address to, bytes calldata data) external returns(uint){
         uint tokenId = counter + 1;
         require(!_isExist[tokenId],"ID exist");
         
         _mint(to, tokenId, 1, data);
         _setApprovalForAll(to, msg.sender, true);
-        fractionalize(to,tokenId,data);
+        fractionalize(to,tokenId,data,msg.sender);
 
         _isExist[tokenId] = true;
         _isInitialized[tokenId] = true;
@@ -54,42 +45,47 @@ contract FNFT is ERC1155 {
         return counter;
     }
 
-    function fractionalize(address _to, uint tokenId, bytes calldata data)internal{    
-        // _mint(msg.sender, tokenId, 9, data);
+    function fractionalize(address _to, uint tokenId, bytes calldata data, address approver)internal{    
+
         uint[] memory ids = new uint256[](9);
         uint[] memory values= new uint256[](9);
-
         unchecked{
             for (uint i = 0; i<9; i++) 
             {
                 ids[i] = tokenId;
                 values[i] = 1;
                 isLocked[tokenId][i]=true;
-
             }
         }
-        // _mint(_to, tokenId, 9, data);
+
         _mintBatch(_to, ids, values, data);
+        _setApprovalForAll(_to, approver, true);
         unchecked{
-            for (uint8 i = 0 ; i<9; i++) 
-            {
-                _ownerOf[tokenId][i] = _to;
+            for (uint8 i = 0 ; i<9; i++){
+            _ownerOf[tokenId][i] = _to;
             }
         }
     }
 
-    function unfractionalize(uint tokenId) external isExist(tokenId) {
+    function unfractionalize(address from, uint tokenId) external isExist(tokenId) {
         require(_isInitialized[tokenId],"Not Fracted");
-        require(msg.sender == _Minter[tokenId],"Not Owner");
-        // require(!_isForSale[tokenId],"NFT is on sale");
-       
-        _burn(msg.sender, tokenId, 9);
+        require(msg.sender == _Minter[tokenId] || isApprovedForAll(from, msg.sender),"Not Owner");
+        uint256[] memory ids = new uint256[](9);
+        uint256[] memory values = new uint256[](9);
+
+        unchecked{
+            for (uint8 i = 0; i<9; i++){
+                ids[i] = tokenId;
+                values[i] =  1;
+            }
+        }
+        _burn(from, tokenId, 9);
         
         _isExist[tokenId] = false;
         _isInitialized[tokenId] = false;
     
         _Minter[tokenId] = address(0);
-        _balanceOf[msg.sender][tokenId] = 0;
+        _balanceOf[from][tokenId] = 0;
         unchecked{
             for (uint8 i = 0; i<9; i++) 
             {
@@ -97,33 +93,30 @@ contract FNFT is ERC1155 {
                 _priceOf[tokenId][i] = 0;
             }
         }
-    }    
+    }
 
-    function approve(uint256 tokenId, uint8 fid, address operator)external isExist(tokenId){
+    function approve(address from,uint256 tokenId, uint8 fid, address operator)external isExist(tokenId){
         require(fid<=8,"FID does not Exist");
         require(operator != address(0),"Invalid Operator");
-        setApprovalForAll(operator, true);
+        _setApprovalForAll(from, msg.sender, true);
     }
 
     function transferFNFT(address from, address to, uint tokenId, uint8 fid) external isExist(tokenId){
-        require(msg.sender == _ownerOf[tokenId][fid] || isApprovedForAll(from, msg.sender),"Not Authorized");
 
         require(from == _ownerOf[tokenId][fid],"From is Not Owner of given NFT");
         require(to!= address(0));
         require(fid <=8,"FID doesnot exist");
+       
         safeTransferFrom(from, to, tokenId, 1, "");
-
+        
         _balanceOf[to][tokenId] +=1;
         _balanceOf[from][tokenId] -=1;
 
         _ownerOf[tokenId][fid]= to;
     }
-    
-    function transferBatch(address from, address to, uint tokenId) external {
-        require(isApprovedForAll(from, msg.sender),"Not Authorized");
-        require(from != address(0),"Invalid Sender");
-        require(to != address(0),"Invalid Receiver");
-        
-        safeTransferFrom(from, to, tokenId, 1, "");
+    function isMinter(uint256 tokenId) external view returns(address minter){
+        if(msg.sender.code.length > 0){
+            minter = _Minter[tokenId];
+        }
     }
 }
